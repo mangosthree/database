2159431d9b62558da0120e6661289f79c551fdd6
bels <bels11@yahoo.com>
2013-11-06 14:41:31 -0500

    [Creature] Fixed some duplicate bosses in Dun Morogh

---
+++ updates/2013_11_06_01_remove_vagash_duplicate.sql
@@ -0,0 +1,2 @@
+-- removes the duplicate vagash that is in Dun Morogh outside of his cave
+delete from creature where guid = 170252;
---
+++ updates/2013_11_06_02_remove_captainbeld_duplicate.sql
@@ -0,0 +1,2 @@
+-- Removes the duplicate Captain Beld so the quest that requires getting to his lockbox is easier/doable
+delete from creature where guid = 170301;
