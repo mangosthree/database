8c97c7969db7d0b556f80213182bde45813f4c98
User Bels <bels@shaggy.belfield.us>
2013-12-28 00:23:24 -0500

    - Removed a duplicate Wonderform Operator in Orgimmar reported in issue #11.

--- updates/mangos_db_update4.sql
+++ updates/mangos_db_update4.sql
@@ -2,3 +2,7 @@
 # Fixes an incorrect drop for Northwatch Scouts in Durotar
 
 DELETE FROM `mangos`.`creature_loot_template` WHERE `creature_loot_template`.`entry` = 39317 AND `creature_loot_template`.`item` = 3264;
+
+# Removes a duplicate Wonderform Operator in Orgrimmar
+
+DELETE FROM `mangos`.`creature` WHERE `creature`.`guid` = 354064;
