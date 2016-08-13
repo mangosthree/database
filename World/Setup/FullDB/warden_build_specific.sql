2652185a8e6ccb21570e61cfaeb40a7aaaf9ce6d
Charles A Edwards <charles.a.edwards@btinternet.com>
2016-08-13 13:30:47 +0100

    Warden tables added
    Warden tables added to characters and workd databases.

---
+++ World/Setup/FullDB/warden_build_specific.sql
@@ -0,0 +1,41 @@
+-- MySQL dump 10.13  Distrib 5.5.37, for Win32 (x86)
+--
+-- Host: localhost    Database: mangos3
+-- ------------------------------------------------------
+-- Server version	5.5.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@SESSION.TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/*Table structure for table `warden_build_specific` */
+
+DROP TABLE IF EXISTS `warden_build_specific`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `warden_build_specific` (
+  `id` smallint(5) unsigned NOT NULL COMMENT 'Check ID from warden_checks',
+  `build` smallint(5) unsigned NOT NULL COMMENT 'Client build',
+  `data` varchar(48) NOT NULL DEFAULT '',
+  `str` varchar(20) NOT NULL DEFAULT '',
+  `address` int(10) unsigned NOT NULL DEFAULT '0',
+  `length` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `result` varchar(24) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`build`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
