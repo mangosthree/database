d152e77081b2fed17f50c04e9507fd80a0ca6703
Necrovoice <dragonsfrost@hotmail.com>
2022-02-19 16:45:15 -0500

    Fix typo

--- World/Updates/Rel21/Rel21_15_013_Feed_of_Evil.sql
+++ World/Updates/Rel21/Rel21_15_013_Feed_of_Evil.sql
@@ -48,7 +48,7 @@ BEGIN
 	UPDATE `creature_template` SET `UnitFlags2` = 2048, `AIName` = 'EventAI' WHERE `Entry` = 37155;
 	UPDATE `creature_template` SET `UnitFlags2` = 2048, `AIName` = 'EventAI' WHERE `Entry` = 37156;
 
-	--Script for Kill credit and Despawn for Trough 1st, 2nd and 3rd.
+	-- Script for Kill credit and Despawn for Trough 1st, 2nd and 3rd.
 	DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (36727,37155,37156);
 	INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
 	(3672701,36727,8,0,100,0, 69228,-1,0,0,33,36727,6,0,0,0,0,0,0,0,0,0,'First Trough - On Spellhit - Quest Credit- 14461'),
